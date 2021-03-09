67e9bc1f1a174cd749b2cc7c043a6754a5d67cd5
Andrey.Speransky <andrey.speransky@gmail.com>
2021-03-09 15:36:52 +0300

    1_Scrum: 1.Создан Django проект, 2.Главная страница, 3.Модель "новости"  4.приложениее "mainapp"

---
+++ mainapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import News
+
+def index(request):
+    print(request)   # <WSGIRequest: GET '/news/'>
+    news = News.objects.all()
+    # news = News.objects.order_by('-created_at')
+    context = {
+        'news': news,
+        'title': 'Список новостей'
+    }
+    # ключи из словарей затем используются в качестве переменных в шаблонах
+    return render(request, 'mainapp/index.html', context)
+    # return render(request, template_name='news/index.html', context=context)
