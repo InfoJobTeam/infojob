40fd903f0afb87521531e2ac5718af56a74b29b0
Andrey.Speransky <andrey.speransky@gmail.com>
2021-03-11 17:46:20 +0300

    2: Добавлена раздача статики, добавлены html шаблоны

--- infojob/settings.py
+++ infojob/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mainapp.apps.MainappConfig',
+    'mainapp.apps.MainappConfig',       # приложение.файл apps.py.класс MainappConfig
 ]
 
 MIDDLEWARE = [
@@ -111,12 +111,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Папка для сбора всей статики
+# python manage.py collectstatic
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
-
-
+# Дополнительные пути со статикой
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static_dev'),
+    os.path.join(BASE_DIR, 'infojob/static'),
 )
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
